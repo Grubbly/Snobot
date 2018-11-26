a48ac70a8bc8aff04953303ba396a3c78e644cf8
unknown <orthogonaltrajectory@gmail.com>
2018-11-25 21:53:15 -0900

    Refactored drive.py code

--- drive.py
+++ drive.py
@@ -30,29 +30,33 @@ def backward(speed):
     backward_pwm.ChangeDutyCycle(speed)
     time.sleep(0.015)
 
-# Wrap in try catch
-while True:
-    command = input("Enter f/r (forward/backward) followed by a number 0-9 (speed) to drive. E.g. f5 :")
-    direction = command[0]
-    if len(command) == 2:
-        print("speed set")
-        speed = int(command[1]) * 11
-    else:
-        print("Invalid command length for speed, speed not set")
-
-    if direction == "f":
-        forward(speed)
-    elif direction == "q":
-        gpio.output(enable, gpio.LOW)
-        gpio.cleanup()
-        print("BYE")
-        break
-    elif direction == " ":
-        power_toggle = not power_toggle
-        if(power_toggle):
-            print("Motor on")
+try:
+    while True:
+        command = input("Enter f/r (forward/backward) followed by a number 0-9 (speed) to drive. E.g. f5 :")
+        direction = command[0]
+        if len(command) == 2 and direction != "q" and direction != " ":
+            print("speed set")
+            speed = int(command[1]) * 11
         else:
-            print("Motor off")
-        gpio.output(enable, power_toggle)
-    else:
-        backward(speed)
+            print("Invalid command length for speed, speed not set")
+
+        if direction == "f":
+            forward(speed)
+        elif direction == "q":
+            gpio.output(enable, gpio.LOW)
+            gpio.cleanup()
+            print("BYE")
+            break
+        elif direction == " ":
+            power_toggle = not power_toggle
+            gpio.output(enable, power_toggle)
+            if(power_toggle):
+                print("Motor on")
+            else:
+                print("Motor off")
+        else:
+            backward(speed)
+except:
+    gpio.output(enable, gpio.LOW)
+    gpio.cleanup()
+    print("BYE")
