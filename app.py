171f0334b94c894c34eb8bdcc765cd15d924dacd
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-11-29 00:21:12 -0900

    First test with driving

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sumNums
+from drive import forward, backward
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -9,7 +10,12 @@ class sumNumbers(Resource):
     def get(self, first, second):
         return {'data': sumNums.sumNums(first,second)}
 
+class driveForward(Resource):
+    def get(self, speed):
+        return {'Motor Data': forward(speed)}
+
 api.add_resource(sumNumbers, '/sumNums/<first>/<second>')
+api.add_resource(driveForward, '/forward/<speed>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
