ed50c49321a000117ce43c4b46ccd577b9559d47
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-11-29 01:56:49 -0900

    Working forward drive, implements variable speed

--- app.py
+++ app.py
@@ -8,20 +8,11 @@ api = Api(app)
 
 @app.route('/')
 def hello_world():
-    snobot_drive.forward(50)
     return 'Hello, World!'
 
-# class sumNumbers(Resource):
-#     def get(self, first, second):
-#         return {'data': sumNums.sumNums(first,second)}
-
-# class driveForward(Resource):
-#    def get(self, speed):
-#        snobot_drive.forward(speed)
-#        return {'Motor Data': "5"}
-
-# api.add_resource(sumNumbers, '/sumNums/<first>/<second>')
-# api.add_resource(driveForward, '/forward/<speed>')
+@app.route('/forward/<speed>')
+    snobot_drive.forward(speed)
+    return 'Driving forward at ' + speed + '%'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
